c55be16c883d0efc9a39777aad43f7d90c0f8db7
Sohail Siadat <sohale@gmail.com>
2015-01-22 15:02:15 +0000

    better output

--- test_decorators.py
+++ test_decorators.py
@@ -23,6 +23,8 @@ def deco1(*args,**kwargs):
           FNC_LIST[f.__name__]=f
           f.args=args
           f.kwargs=kwargs
+      else:
+          print "> >  d1.dec>   on (%s)"%(repr(f),)
       #return f
       def dec3_innermost(x):
          	print "> > >   dec3_innermost: %s"%(repr(x),)
@@ -61,29 +63,31 @@ def begin(p1,p2=-1):
 #    pass
 
 print "1111111"
-start("p1") #acually innermost is called
+start("p1") #acually innermost (dec3_innermost) is called
 print "2222222"
-begin("a1") #acually innermost is called
+begin("a1") #acually innermost () is called
 print "3333333"
 #stop("p1")
 
 
-"""
+""" Output:
+
 >   d1: decorator 1 called on:
 >   d1:     args= ()
 >   d1:     kwargs= {}
-> >  d1.dec> inner decorator is called on f=<function start at 0x7f820ca46de8>
+> >  d1.dec> inner decorator is called on f=<function start at 0x7f91b5a02de8>
 <type 'function'>
 > >  d1.dec> f.__name__ = 'start'
 **********************
  -- 
 >   d1: decorator 1 called on:
->   d1:     args= (<function begin at 0x7f820ca46d70>,)
+>   d1:     args= (<function begin at 0x7f91b5a02d70>,)
 >   d1:     kwargs= {}
 1111111
 > > >   dec3_innermost: 'p1'
 2222222
 > >  d1.dec> inner decorator is called on f='a1'
 <type 'str'>
+> >  d1.dec>   on ('a1')
 3333333
 """
