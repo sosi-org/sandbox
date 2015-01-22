5ad6be59f3c99a6a90a954b54bf12ce60481fd3f
Sohail Siadat <sohale@gmail.com>
2015-01-22 14:58:40 +0000

    more throrough test on decorators

--- test_decorators.py
+++ test_decorators.py
@@ -1,4 +1,5 @@
-COMMAND_LINE_OPTIONS={}
+import inspect
+FNC_LIST={}
 
 class InvalidCommandLine(Exception):
     pass
@@ -8,27 +9,81 @@ def decor2(g):
     return g
 
 
-def command_line_option(*args,**kwargs):
-   print ">   d1: decorator 1 called on ?"
-   print ">   d1: args= %s"%(repr(args),)
-   print ">   d1: args= %s"%(repr(kwargs),)
-   def dec(f):
-      print ">>  d1.dec> inner decorator is called on f=%s"%(repr(f),)
-      print ">>  d1.dec> f.__name__ = %s"%(repr(f.__name__),)
-      f.__name__
-      COMMAND_LINE_OPTIONS[f.__name__]=f
-      f.args=args
-      f.kwargs=kwargs
-      return f
-   return dec
+def deco1(*args,**kwargs):
+   print ">   d1: decorator 1 called on:"
+   print ">   d1:     args= %s"%(repr(args),)
+   print ">   d1:     kwargs= %s"%(repr(kwargs),)
+   def dec2_inner(f):
+      print "> >  d1.dec> inner decorator is called on f=%s"%(repr(f),)
+      print type(f)
+      if type(f) is inspect.types.FunctionType:
+          print "> >  d1.dec> f.__name__ = %s"%(repr(f.__name__),)
+          print "**********************"
+          f.__name__
+          FNC_LIST[f.__name__]=f
+          f.args=args
+          f.kwargs=kwargs
+      #return f
+      def dec3_innermost(x):
+         	print "> > >   dec3_innermost: %s"%(repr(x),)
+         	pass
+      return dec3_innermost
+   return dec2_inner
 
 
-@command_line_option() #the decorator is actually returned by this
-def start(child_target_uid,pipeio_with_scheduler,worker_id=-1):
-    print "start is called"
+#if @deco1 ---> dec is not called
+#if @deco1() ---> dec Is called
+
+
+#USING () :
+# @deco1()
+#the decorator is actually returned by this
+
+
+#NOT USING ()
+# calls def on the function's parameters. Calls dec each time the function is called. The main ddddddddd is called once, with args (f)
+#
+
+@deco1()
+def start(p1,p2=-1):
+    print "**** start() is called"
     pass
 
-@decor2
-def stop(child_target_uid,pipeio_with_scheduler,worker_id=-1):
-    print "start is called"
+print " -- "
+@deco1
+def begin(p1,p2=-1):
+    print "***** begin() is called"
     pass
+
+#@decor2
+#def stop(child_target_uid,worker_id=-1):
+#    print "stop is called"
+#    pass
+
+print "1111111"
+start("p1") #acually innermost is called
+print "2222222"
+begin("a1") #acually innermost is called
+print "3333333"
+#stop("p1")
+
+
+"""
+>   d1: decorator 1 called on:
+>   d1:     args= ()
+>   d1:     kwargs= {}
+> >  d1.dec> inner decorator is called on f=<function start at 0x7f820ca46de8>
+<type 'function'>
+> >  d1.dec> f.__name__ = 'start'
+**********************
+ -- 
+>   d1: decorator 1 called on:
+>   d1:     args= (<function begin at 0x7f820ca46d70>,)
+>   d1:     kwargs= {}
+1111111
+> > >   dec3_innermost: 'p1'
+2222222
+> >  d1.dec> inner decorator is called on f='a1'
+<type 'str'>
+3333333
+"""
